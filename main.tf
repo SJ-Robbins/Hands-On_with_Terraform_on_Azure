288a682de189dee66a397c02003608e3255e52f8
Steve Robbins <steven@therobbinsfamily.co.uk>
2023-08-20 18:51:34 +0000

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-d75a68f6-hands-on-with-terraform-on-azure"
+  location = "East US"
+}
+
+module "securestorage" {
+  source               = "app.terraform.io/sj-robbins-org/securestorage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "sjrstorage001a"
+}
